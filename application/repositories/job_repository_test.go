31058d5dc4779f0be18f2730a052018c4a18a549
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2024-01-20 13:21:24 -0300

    test: add teste to JobRepository Updated

--- application/repositories/job_repository_test.go
+++ application/repositories/job_repository_test.go
@@ -1,6 +1,7 @@
 package repositories_test
 
 import (
+	"github.com/jinzhu/gorm"
 	"github.com/olukkas/go-encoder/application/repositories"
 	"github.com/olukkas/go-encoder/domain"
 	"github.com/olukkas/go-encoder/framework/database"
@@ -12,12 +13,42 @@ func TestJobRepositoryDb_Insert(t *testing.T) {
 	db := database.NewDataBaseTest()
 	defer db.Close()
 
+	jobRepo := repositories.NewJobRepositoryDb(db)
+
+	job := prepareJobHelper(t, db)
+
+	_, err := jobRepo.Insert(job)
+	require.Nil(t, err)
+
+	j, err := jobRepo.Find(job.ID)
+	require.Nil(t, err)
+	require.NotEmpty(t, j.ID)
+	require.Equal(t, j.ID, job.ID)
+	require.NotEmpty(t, j.VideoId)
+}
+
+func TestJobRepositoryDb_Update(t *testing.T) {
+	db := database.NewDataBaseTest()
+	defer db.Close()
+
+	jobRepo := repositories.NewJobRepositoryDb(db)
+	job := prepareJobHelper(t, db)
+
+	_, err := jobRepo.Insert(job)
+	require.Nil(t, err)
+
+	job.Status = domain.JobDownloading
+	updated, err := jobRepo.Update(job)
+	require.Nil(t, err)
+	require.Equal(t, domain.JobDownloading, updated.Status)
+}
+
+func prepareJobHelper(t *testing.T, db *gorm.DB) *domain.Job {
 	video, err := domain.NewVideo("resource", "path")
 	require.Nil(t, err)
 	require.NotNil(t, video)
 
 	videoRepo := repositories.NewVideoRepositoryDb(db)
-	jobRepo := repositories.NewJobRepositoryDb(db)
 
 	_, err = videoRepo.Insert(video)
 	require.Nil(t, err)
@@ -25,12 +56,5 @@ func TestJobRepositoryDb_Insert(t *testing.T) {
 	job, err := domain.NewJob("output_path", domain.JobPending, video)
 	require.Nil(t, err)
 
-	_, err = jobRepo.Insert(job)
-	require.Nil(t, err)
-
-	j, err := jobRepo.Find(job.ID)
-	require.Nil(t, err)
-	require.NotEmpty(t, j.ID)
-	require.Equal(t, j.ID, job.ID)
-	require.Equal(t, j.VideoId, video.ID)
+	return job
 }
