23af97d72dfb4a4b0148c69d16e840a9a9093181
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2024-01-21 13:44:33 -0300

    test: add tests to upload_manager

---
+++ application/services/upload_manager_test.go
@@ -0,0 +1,48 @@
+package services_test
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/olukkas/go-encoder/application/services"
+	"github.com/stretchr/testify/require"
+	"log"
+	"os"
+	"testing"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+//goland:noinspection GoUnhandledErrorResult
+func TestVideoServiceUpload(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.VideoService{}
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("go-encoder-bucket")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "go-encoder-bucket"
+	videoUpload.VideoPath = os.Getenv("LOCAL_STORAGE_PATH") + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(10, doneUpload)
+
+	result := <-doneUpload
+	require.Equal(t, result, "upload completed")
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
